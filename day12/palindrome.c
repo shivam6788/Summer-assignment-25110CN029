92db0eb61ff266f7dd2d6aa0dff9872852178c14
shivam6788 <sonkarshivam672@gmail.com>
2026-06-18 09:38:16 +0530

    Add palindrome checking functionality in C

---
+++ day12/palindrome.c
@@ -0,0 +1,27 @@
+#include<string.h>
+int isPalindrome(char str[]) {
+    int left = 0;
+    int right = strlen(str) - 1;
+
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return 0; // Not a palindrome
+        }
+        left++;
+        right--;
+    }
+    return 1; // Is a palindrome
+}
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    
+    if (isPalindrome(str)) {
+        printf("%s is a palindrome.\n", str);
+    } else {
+        printf("%s is not a palindrome.\n", str);
+    }
+    
+    return 0;
+}
