87eee4ccd972c0ae8ada05e161e9fd5730719af8
shivam6788 <sonkarshivam672@gmail.com>
2026-06-18 09:37:37 +0530

    Add Fibonacci sequence calculation in C

---
+++ day12/fibonnaci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fibonacci(int n) {
+    if (n <= 0) {
+        return 0; 
+    } else if (n == 1) {
+        return 1; 
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2); 
+    }
+}
+int main() {
+    int n;
+    printf("Enter the position in Fibonacci sequence: ");
+    scanf("%d", &n);
+    
+    int result = fibonacci(n);
+    printf("The %dth Fibonacci number is: %d\n", n, result);
+    
+    return 0;
+}
