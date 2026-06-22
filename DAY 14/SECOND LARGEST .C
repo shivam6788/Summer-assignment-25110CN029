93dfb2ed41a67188c068913feb54cf4cad50d503
shivam6788 <sonkarshivam672@gmail.com>
2026-06-22 09:55:34 +0530

    Add program to find the second largest number

---
+++ DAY 14/SECOND LARGEST .C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() {
+    int arr[20];
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    for (int i=0; i<n; i++){
+        printf("Enter number %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    int largest = arr[0];
+    int second_largest = arr[0];
+    for (int i=1; i<n; i++){
+        if (arr[i] > largest) {
+            second_largest = largest;
+            largest = arr[i];
+        } else if (arr[i] > second_largest && arr[i] != largest) {
+            second_largest = arr[i];
+        }
+    }
+    printf("The second largest number is: %d\n", second_largest);
+    return 0;
+}
